c140579cfdbbd4d4cb4d27ef359f3a8629895102
samcday <me@samcday.com.au>
2013-06-27 12:15:08 +1000

    stfu lint errors.

--- test/readable_streambuffer.test.js
+++ test/readable_streambuffer.test.js
@@ -9,23 +9,23 @@ vows.describe("ReadableStreamBuffer").addBatch({
 		topic: function() {
 			return new streamBuffer.ReadableStreamBuffer();
 		},
-		
+
 		"is a Stream": function(aStreamBuffer) {
 			assert.instanceOf(aStreamBuffer, require("stream").Stream);
 		},
-		
+
 		"is readable": function(aStreamBuffer) {
 			assert.isTrue(aStreamBuffer.readable);
 		},
-		
+
 		"is not writable": function(aStreamBuffer) {
 			assert.isFalse(aStreamBuffer.writable);
 		},
-		
+
 		"is empty": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer.size(), 0);
 		},
-		
+
 		"has default backing buffer size": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer.maxSize(), streamBuffer.DEFAULT_INITIAL_SIZE);
 		}
@@ -38,26 +38,26 @@ vows.describe("ReadableStreamBuffer").addBatch({
 			aStreamBuffer.put(fixtures.simpleString);
 			return aStreamBuffer;
 		},
-		
+
 		"has correct size": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer.size(), fixtures.simpleString.length);
 		},
-		
+
 		"has correct backing buffer size": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer.maxSize(), streamBuffer.DEFAULT_INITIAL_SIZE);
 		},
-		
+
 		"and then resumed, with encoding set to utf8": {
 			topic: function(aStreamBuffer) {
 				aStreamBuffer.setEncoding("utf8");
 				aStreamBuffer.on("data", this.callback.bind(this, null));
 				aStreamBuffer.resume();
 			},
-			
+
 			"results in a String": function(data) {
 				assert.isString(data);
 			},
-			
+
 			"with correct contents": function(data) {
 				assert.equal(data, fixtures.simpleString);
 			}
@@ -68,19 +68,19 @@ vows.describe("ReadableStreamBuffer").addBatch({
 		topic: function() {
 			var aStreamBuffer = new streamBuffer.ReadableStreamBuffer();
 			aStreamBuffer.on("data", this.callback.bind(this, null));
-			
+
 			aStreamBuffer.put(fixtures.binaryData);
 		},
-		
+
 		"results in a Buffer": function(data) {
 			assert.instanceOf(data, Buffer);
 		},
-		
+
 		"with the correct data": function(data) {
 			helpers.assertBuffersEqual(data, fixtures.binaryData);
 		}
 	},
-	
+
 	"Writing binary data larger than initial backing buffer size": {
 		topic: function() {
 			var aStreamBuffer = new streamBuffer.ReadableStreamBuffer();
@@ -88,33 +88,33 @@ vows.describe("ReadableStreamBuffer").addBatch({
 			aStreamBuffer.put(fixtures.largeBinaryData);
 			return aStreamBuffer;
 		},
-		
+
 		"buffer is correct size": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer.size(), fixtures.largeBinaryData.length);
 		},
-		
+
 		"backing buffer is correct size": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer.maxSize(), streamBuffer.DEFAULT_INITIAL_SIZE + streamBuffer.DEFAULT_INCREMENT_AMOUNT);
 		}
 	},
-	
+
 	"Setting custom chunk size": {
 		topic: function() {
 			var that = this;
-			
+
 			var aStreamBuffer = new streamBuffer.ReadableStreamBuffer({
 				chunkSize: 2
 			});
-			
+
 			aStreamBuffer.once("data", function(data) { that.callback(null, data); });
 			aStreamBuffer.put(fixtures.binaryData);
 		},
-		
+
 		"gives us a Buffer with the correct length": function(data) {
 			assert.equal(data.length, 2);
 		}
 	},
-	
+
 	"Setting a custom frequency": {
 		topic: function() {
 			var that = this,
@@ -123,18 +123,18 @@ vows.describe("ReadableStreamBuffer").addBatch({
 			var aStreamBuffer = new streamBuffer.ReadableStreamBuffer({
 				frequency: 300
 			});
-			
+
 			aStreamBuffer.on("data", function(data) { that.callback(null, new Date().getTime() - startTime); });
 			aStreamBuffer.put(fixtures.binaryData);
 		},
-		
+
 		"gave us data after the correct amount of time": function(time) {
 			// Wtfux: sometimes the timer is coming back a millisecond or two
 			// faster. So we do a "close-enough" assertion here ;)
 			assert.isTrue(time >= 295);
 		}
 	},
-	
+
 	"Setting a custom initial size and increment amount": {
 		topic: function() {
 			return new streamBuffer.ReadableStreamBuffer({
@@ -146,54 +146,54 @@ vows.describe("ReadableStreamBuffer").addBatch({
 		"gives us correct initial backing buffer size": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer.maxSize(), 1);
 		},
-		
+
 		"and writing to size of initial size": {
 			topic: function(aStreamBuffer) {
 				aStreamBuffer.put("ab");
 				return aStreamBuffer;
 			},
-			
+
 			"gives us correct incremented size of backing buffer": function(aStreamBuffer) {
 				assert.equal(aStreamBuffer.maxSize(), 6);
 			}
 		}
 	},
-	
+
 	"Destroying stream": {
 		topic: function() {
 			var that = this;
-			
+
 			var aStreamBuffer = new streamBuffer.ReadableStreamBuffer();
-			
+
 			aStreamBuffer.on("data", function() {
 				aStreamBuffer.dataCalled = true;
 			});
-			
+
 			aStreamBuffer.on("end", function() {
 				aStreamBuffer.endCalled = true;
 			});
-			
+
 			aStreamBuffer.on("close", function() {
 				that.callback(null, aStreamBuffer);
 			});
-			
+
 			aStreamBuffer.put("asdf");
 			aStreamBuffer.destroy();
 		},
-		
+
 		"sets *readable* to false": function(aStreamBuffer) {
 			assert.isFalse(aStreamBuffer.readable);
 		},
-		
+
 		"*data* event was never called": function(aStreamBuffer) {
 			assert.isFalse(aStreamBuffer.dataCalled || false);
 		},
-		
+
 		"*end* event was called": function(aStreamBuffer) {
 			assert.isTrue(aStreamBuffer.endCalled);
 		}
 	},
-	
+
 	"Data written in two chunks": {
 		topic: function() {
 			var that = this;
@@ -202,13 +202,13 @@ vows.describe("ReadableStreamBuffer").addBatch({
 				chunkSize: Math.ceil(fixtures.simpleString.length / 2)
 			});
 			aStreamBuffer.setEncoding("utf8");
-			
+
 			var chunks = [];
 			aStreamBuffer.on("data", function(data) {
 				chunks.push(data);
 				if(chunks.length == 2) that.callback(null, chunks);
 			});
-			
+
 			aStreamBuffer.put(fixtures.simpleString);
 		},
 
@@ -216,7 +216,7 @@ vows.describe("ReadableStreamBuffer").addBatch({
 			assert.equal(chunks[0] + chunks[1], fixtures.simpleString);
 		}
 	},
-	
+
 	"Writing unicode data in two writes": {
 		topic: function() {
 			var that = this;
--- test/writable_streambuffer.test.js
+++ test/writable_streambuffer.test.js
@@ -9,26 +9,26 @@ vows.describe("WritableStreamBuffer").addBatch({
 		topic: function() {
 			return new streamBuffer.WritableStreamBuffer();
 		},
-		
+
 		"is writable": function(aStreamBuffer) {
 			assert.isTrue(aStreamBuffer.writable);
 		},
-		
+
 		"is not readable": function(aStreamBuffer) {
 			assert.isFalse(aStreamBuffer.readable);
 		},
-		
+
 		"calling *getContents()* false when empty": function(aStreamBuffer) {
 			assert.isFalse(aStreamBuffer.getContents());
 		},
-		
+
 		"calling *getContentsAsString()* returns false when empty": function(aStreamBuffer) {
 			assert.isFalse(aStreamBuffer.getContentsAsString());
 		},
-		
+
 		"backing buffer should be default size": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer.maxSize(), streamBuffer.DEFAULT_INITIAL_SIZE);
-		},
+		}
 	},
 
 	"Writing a simple string": {
@@ -41,11 +41,11 @@ vows.describe("WritableStreamBuffer").addBatch({
 		"backing buffer should be correct length": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer.size(), fixtures.simpleString.length);
 		},
-		
+
 		"max size should be default size": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer.maxSize(), streamBuffer.DEFAULT_INITIAL_SIZE);
 		},
-		
+
 		"contents should be correct": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer.getContentsAsString(), fixtures.simpleString);
 		}
@@ -57,34 +57,34 @@ vows.describe("WritableStreamBuffer").addBatch({
 			aStreamBuffer.write(fixtures.largeBinaryData);
 			return aStreamBuffer;
 		},
-		
+
 		"backing buffer should be correct size": function(aStreamBuffer) {
-			assert.equal(aStreamBuffer.size(), fixtures.largeBinaryData.length)
+			assert.equal(aStreamBuffer.size(), fixtures.largeBinaryData.length);
 		},
-		
+
 		"backing buffer size should have incremented": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer.maxSize(), streamBuffer.DEFAULT_INITIAL_SIZE + streamBuffer.DEFAULT_INCREMENT_AMOUNT);
 		},
-		
+
 		"contents are valid": function(aStreamBuffer) {
 			helpers.assertBuffersEqual(aStreamBuffer.getContents(), fixtures.largeBinaryData);
 		}
 	},
-	
+
 	"When writing some simple data to the stream": {
-		topic: function(aStreamBuffer) {
+		topic: function() {
 			var aStreamBuffer = new streamBuffer.WritableStreamBuffer();
 			aStreamBuffer.write(fixtures.simpleString);
 			return aStreamBuffer;
 		},
-		
+
 		"and retrieving half of it": {
 			topic: function(aStreamBuffer) {
 				var str = aStreamBuffer.getContentsAsString("utf8", Math.floor(fixtures.simpleString.length / 2));
 				aStreamBuffer.testStr = str;
 				return aStreamBuffer;
 			},
-			
+
 			// No clue why, but first param is a null value...
 			"we get correct data": function(wtf, aStreamBuffer) {
 				assert.equal(aStreamBuffer.testStr, fixtures.simpleString.substring(0, Math.floor(fixtures.simpleString.length / 2)));
@@ -94,7 +94,7 @@ vows.describe("WritableStreamBuffer").addBatch({
 			"and there is still correct amount of data remaining in stream buffer": function(wtf, aStreamBuffer) {
 				assert.equal(aStreamBuffer.size(), Math.ceil(fixtures.simpleString.length / 2));
 			},
-			
+
 			// Not very well documented (IMO), but sub contexts *can* execute asynchronously. So I put this context here to make sure it executes after
 			// the first half of data is read.
 			"and then retrieving the other half of it": {
@@ -103,11 +103,11 @@ vows.describe("WritableStreamBuffer").addBatch({
 					aStreamBuffer.testStr = str;
 					return aStreamBuffer;
 				},
-				
+
 				"we get correct data": function(wtf, aStreamBuffer) {
 					assert.equal(aStreamBuffer.testStr, fixtures.simpleString.substring(Math.floor(fixtures.simpleString.length / 2)));
 				},
-				
+
 				"and stream buffer is now empty": function(wtf, aStreamBuffer) {
 					assert.equal(aStreamBuffer.size(), 0);
 				}
@@ -123,23 +123,23 @@ vows.describe("WritableStreamBuffer").addBatch({
 			});
 			return aStreamBuffer;
 		},
-		
+
 		"has the correct initial size": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer.maxSize(), 62);
 		},
-		
+
 		"after data is written": {
 			topic: function(aStreamBuffer) {
 				aStreamBuffer.write(fixtures.binaryData);
 				return aStreamBuffer;
 			},
-			
+
 			"has correct initial size + custom increment amount": function(aStreamBuffer) {
 				assert.equal(aStreamBuffer.maxSize(), 321 + 62);
 			}
 		}
 	},
-	
+
 	"When stream is written in two chunks": {
 		topic: function() {
 			var aStreamBuffer = new streamBuffer.WritableStreamBuffer();
@@ -147,12 +147,12 @@ vows.describe("WritableStreamBuffer").addBatch({
 			aStreamBuffer.write(fixtures.simpleString);
 			return aStreamBuffer;
 		},
-		
+
 		"buffer contents are correct": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer.getContentsAsString(), fixtures.simpleString + fixtures.simpleString);
 		}
 	},
-	
+
 	"When stream is *end()*'ed with final buffer": {
 		topic: function() {
 			var aStreamBuffer = new streamBuffer.WritableStreamBuffer();
@@ -164,7 +164,7 @@ vows.describe("WritableStreamBuffer").addBatch({
 		"buffer contents are correct": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer.getContentsAsString(), fixtures.simpleString + fixtures.simpleString);
 		},
-		
+
 		"writable should be false": function(aStreamBuffer) {
 			assert.isFalse(aStreamBuffer.writable);
 		}
@@ -177,11 +177,11 @@ vows.describe("WritableStreamBuffer").addBatch({
 			aStreamBuffer.destroy(fixtures.simpleString);
 			return aStreamBuffer;
 		},
-		
+
 		"buffer contents are correct": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer.getContentsAsString(), fixtures.simpleString);
 		},
-		
+
 		"stream is no longer writable": function(aStreamBuffer) {
 			assert.isFalse(aStreamBuffer.writable);
 		}
@@ -190,13 +190,13 @@ vows.describe("WritableStreamBuffer").addBatch({
 	"destroySoon() on stream": {
 		topic: function() {
 			var that = this;
-			
+
 			var aStreamBuffer = new streamBuffer.WritableStreamBuffer();
 
 			aStreamBuffer.on("close", function() {
 				that.callback(null, aStreamBuffer);
 			});
-			
+
 			aStreamBuffer.write(fixtures.simpleString);
 			aStreamBuffer.destroySoon();
 		},
@@ -204,24 +204,24 @@ vows.describe("WritableStreamBuffer").addBatch({
 		"sets *writable* to false": function(aStreamBuffer) {
 			assert.isFalse(aStreamBuffer.writable);
 		},
-		
+
 		"correct data is in buffer": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer.getContentsAsString(), fixtures.simpleString);
 		}
 	},
-	
+
 	"Writing data after destroySoon()": {
 		topic: function() {
 			var aStreamBuffer = new streamBuffer.WritableStreamBuffer();
 			aStreamBuffer.destroySoon();
 			aStreamBuffer.write(fixtures.basicString);
-			
+
 			// Check size of buffer now, as vows may go do other stuff.
 			aStreamBuffer._bufferSize = aStreamBuffer.size();
-			
+
 			return aStreamBuffer;
 		},
-		
+
 		"should be silently ignored": function(aStreamBuffer) {
 			assert.equal(aStreamBuffer._bufferSize, 0);
 		}
